3ca3d2cd4baa26e0999af6632b1cf2dbd4092096
Juliano Campos <88381643+julianocamposcode@users.noreply.github.com>
2024-01-28 17:12:47 -0300

    Update main.js

--- rio branco clone/js/main.js
+++ rio branco clone/js/main.js
@@ -1795,8 +1795,9 @@ img_junior.forEach(imagem => {
 
     const srcAtual = imagem.getAttribute("src");
 
-    if (srcAtual === 'img/welcome_image.jpg') {
+if (srcAtual === 'img/welcome_image.jpg') {
         const novoSrc = srcAtual.replace('.jpg', '.png');
         imagem.setAttribute("src", novoSrc);
-    }
-});
+		imagem.style.marginLeft = '10em';
+		imagem.id = 'junior'
+	}});
